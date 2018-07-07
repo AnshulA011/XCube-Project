67ba9e3fc11e72c2fd83d43e0c3ffd7d8de74a3a
AnshulA011 <31186380+AnshulA011@users.noreply.github.com>
2018-07-07 11:50:46 -0400

    Opening file

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(void) {
+  FILE *filePtr;
+
+  filePtr = fopen("code.asm", "r");
+
+  if (filePtr == NULL){
+    printf("Could not find file!");
+    fclose(filePtr);
+  }
+  else{
+    char line[128];
+    while(fgets(line, sizeof(line), filePtr))
+    {
+      
+      printf("%c\n", line[0]);
+    } 
+
+
+    fclose(filePtr);
+  }
+
+  return 0;
+}
